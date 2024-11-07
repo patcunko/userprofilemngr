7861d59870032dc7817c50de7a5d83c77a10caf7
andrziv <soveryhappyface@gmail.com>
2024-11-07 00:23:22 -0500

    Code cleanup, visuals cleanup
    Cleaned up the code
    
    Cleaned up the visuals so that elements are better centered

--- src/components/profile.js
+++ src/components/profile.js
@@ -8,11 +8,45 @@ import LoginContext from "../context/login-context";
 
 const ListVehicle = (props) => {
 	return (
-		<div className="flex rounded-lg bg-white border grow w-full h-30 p-5 space-y-1 hover:bg-gray-200">
-			<div className="flex grid grid-cols-10 col-span-4 space-x-1 rtl:space-x-reverse">
-				<label className="text-xl font-semibold whitespace-nowrap flex col-span-3 dark:text-black">{props.vehicle.make}</label>
-				<label className="text-xl font-semibold whitespace-nowrap flex col-span-6 dark:text-black">{props.vehicle.model}</label>
-				<label className="text-xl font-semibold whitespace-nowrap dark:text-black">{props.vehicle.model_year}</label>
+		<div className="rounded-lg bg-white border p-5 hover:bg-gray-200">
+			<label className="text-xl font-semibold whitespace-nowrap text-black">{props.vehicle.make} {props.vehicle.model} {props.vehicle.model_year}</label>
+		</div>
+	);
+}
+
+function CompanyDetails(user) {
+	return (
+		<div className='rounded-2xl shadow-md'>
+			<h2 className='font-bold text-center'>
+				Details
+			</h2>
+			<div className="border-t border-gray-200">
+				<dl>
+					<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
+						<dt className="text-sm font-medium text-gray-500">
+							Company Name: 
+						</dt>
+						<dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+							{user.company.name}
+						</dd>
+					</div>
+					<div className="bg-white px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
+						<dt className="text-sm font-medium text-gray-500">
+							Your Current Position at Company: 
+						</dt>
+						<dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+							{user.role}
+						</dd>
+					</div>
+					<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
+						<dt className="text-sm font-medium text-gray-500">
+							Company Loyalty Points: 
+						</dt>
+						<dd className="mt-1 text-sm text-green-500 font-bold sm:mt-0">
+							{user.company.loyalty_points}
+						</dd>
+					</div>
+				</dl>
 			</div>
 		</div>
 	);
@@ -32,19 +66,19 @@ function PaymentForm() {
 			<form onSubmit={onSubmit} className='pl-6'>
 				<div className="grid grid-cols-2 gap-6">
 					<div className="col-span-2 sm:col-span-1">
-						<label for="card-number" className="block text-sm font-medium text-gray-700 mb-2">Card Number</label>
+						<label htmlFor="card-number" className="block text-sm font-medium text-gray-700 mb-2">Card Number</label>
 						<input type="text" name="card-number" id="card-number" placeholder="0000 0000 0000 0000" className="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"/>
 					</div>
 					<div className="col-span-2 sm:col-span-1">
-						<label for="expiration-date" className="block text-sm font-medium text-gray-700 mb-2">Expiration Date</label>
+						<label htmlFor="expiration-date" className="block text-sm font-medium text-gray-700 mb-2">Expiration Date</label>
 						<input type="text" name="expiration-date" id="expiration-date" placeholder="MM / YY" className="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"/>
 					</div>
 					<div className="col-span-2 sm:col-span-1">
-						<label for="cvv" className="block text-sm font-medium text-gray-700 mb-2">CVV</label>
+						<label htmlFor="cvv" className="block text-sm font-medium text-gray-700 mb-2">CVV</label>
 						<input type="text" name="cvv" id="cvv" placeholder="000" className="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"/>
 					</div>
 					<div className="col-span-2 sm:col-span-1">
-						<label for="card-holder" className="block text-sm font-medium text-gray-700 mb-2">Card Holder</label>
+						<label htmlFor="card-holder" className="block text-sm font-medium text-gray-700 mb-2">Card Holder</label>
 						<input type="text" name="card-holder" id="card-holder" placeholder="Full Name" className="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"/>
 					</div>
 				</div>
@@ -89,11 +123,6 @@ function ContactInfoForm() {
 export default function ProfilePage() {
 	// eslint-disable-next-line
 	const [isLoggedIn, setIsLoggedIn, user, setUser] = useContext(LoginContext);
-	const navigate = useNavigate();
-
-	if (isLoggedIn === false) {
-		navigate('/');
-	}
 
 	function vehicleList() {
 		return getOwnedVehicles(user).map((vehicle) => {
@@ -106,56 +135,26 @@ export default function ProfilePage() {
 	}
 
 	return (
-		// absolute top-0 
-		<div className="grid space-y-4 justify-items-center">
-			
-			<div className="shadow-2xl justify-self-center bg-green-300 text-center align-middle text-black font-bold py-1 mb-8 w-full h-14">
+		<div className="grid space-y-6 justify-items-center">
+			<div className="shadow-2xl bg-green-300 text-center align-middle text-black font-bold text-lg py-4 w-full h-14">
 				Welcome, {user.first_name} {user.last_name}!
 			</div>
 
-			<div className="self-center rounded-2xl shadow-2xl bg-white justify-self-center items-center w-5/6 h-50 space-y-4 pt-6">
+			<div className="rounded-2xl shadow-2xl bg-white w-5/6 h-50 space-y-4 pt-6">
 				<div className='border-l-8 border-red-400 bg-gray-100 font-bold pl-6 mb-6 w-1/4'>
 					<h2>
 						Company Information
 					</h2>
 				</div>
-				<div className='rounded-2xl shadow-md'>
-					<h2 className='font-bold text-center'>
-						Details
-					</h2>
-					<div className='grid w-1/2 justify-self-center'>
-						<div className='flex'>
-							<h2 className="rounded-2xl bg-white h-full p-4">
-								Your current role at the company: 
-							</h2>
-							<h2 className="rounded-2xl bg-white h-full p-4">
-								Your current role at the company: 
-							</h2>
-							<label className="rounded-2xl bg-white h-full p-4">
-								{user.role}
-							</label>
-						</div>
-
-						<div className='flex w-1/2 justify-self-center'>
-							<h2 className="rounded-2xl bg-white h-full p-4">
-							Company Loyalty Points: 
-							</h2>
-							<label className="rounded-2xl bg-white h-full p-4">
-							{user.company.loyalty_points}
-							</label>
-						</div>
-					</div>
-				</div>
-
+				{CompanyDetails(user)}
 				<div>
 					<h2 className='font-bold text-center'>
 						Registered Fleet Vehicles
 					</h2>
-					<div className="self-center rounded-2xl bg-gray-800 justify-self-center items-center w-full h-full p-4 space-y-2">
+					<div className="rounded-2xl bg-gray-800 p-4 space-y-2">
 						{vehicleList()}
 					</div>
 				</div>
-
 			</div>
 			<div className='grid grid-cols-2 gap-6 w-5/6'>
 				{PaymentForm()}
